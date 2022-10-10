966c3405d534616e0a58fcba719629154322291f
Chibs5 <76573206+Chibs5@users.noreply.github.com>
2022-10-11 00:52:48 +0200

    Added a profile layout

--- app/src/main/java/com/example/tutor2/MainActivity.java
+++ app/src/main/java/com/example/tutor2/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.tutor2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = findViewById(R.id.imagep);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
